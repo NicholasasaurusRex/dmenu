87f55320b1b969d7f08883369d325f47c8016abc
NicholasasaurusRex <nicholasasaurusrex@gmail.com>
2020-04-29 02:29:37 -0400

    colorscheme now controlled by all suckless programs

--- config.h
+++ config.h
@@ -4,23 +4,35 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10",
-	"JoyPixels:pixelsize=8:antialias=true:autohint=true"
+/*	"monospace:size=10", */
+/*	"JoyPixels:pixelsize=8:antialias=true:autohint=true", */
+	"Source Code Pro:size=10",
+	"Source Code Pro:pixelsize=8:antialias=true:autohint=true",
+
 };
 static const unsigned int bgalpha = 0xe0;
 static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	/*                      fg         bg            fg        bg        */
+	[SchemeNorm]  =   { "#ffffff", "#000000" }, /* #bbbbbb  #222222 */
+	[SchemeSel]   =   { "#000000", "#afd787" }, /* #eeeeee  #005577 */
+	[SchemeOut]   =   { "#000000", "#000000" }, /* #000000  #00ffff */
 };
+
+/* Color Scheme */
+/* *.color0: #000000 */
+/* *.color2: #d78787 */
+/* *.color3: #ffb76d */
+/* *.color4: #87afd7 */
+/* *.color5: #d7afd7 */
+/* *.color6: #afd787 */
+
 static const unsigned int alphas[SchemeLast][2] = {
-	/*		fgalpha		bgalphga	*/
-	[SchemeNorm] = { fgalpha, bgalpha },
-	[SchemeSel] = { fgalpha, bgalpha },
-	[SchemeOut] = { fgalpha, bgalpha },
+	/*		              fgalpha	  bgalphga	    */
+	[SchemeNorm]  =   { fgalpha, bgalpha },
+	[SchemeSel]   =   { fgalpha, bgalpha },
+	[SchemeOut]   =   { fgalpha, bgalpha },
 };
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
